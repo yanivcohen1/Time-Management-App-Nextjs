2a853ec8c5325e1b67d1480047e97ef12ef7ffb0
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-11-23 19:47:53 +0200

    up tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -18,5 +18,12 @@ describe('My Next App', () => {
     cy.contains('Log in').click();
     cy.url().should('include', '/login');
     cy.get('#username').should('be.visible'); // Check for the username input
+    cy.get('span.rt-r-size-2').click();
+    cy.get('#username').click();
+    cy.get('#username').type('admin');
+    cy.get('#password').click();
+    cy.get('#password').type('admin123');
+    cy.get('button.rt-reset').click();
+    cy.get('a[href="/admin/1"] span').should('be.visible');
   });
 });
